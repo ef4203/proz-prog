a7512291312ac4385b96d29830bcb3c941ddd58d
Elias F <elias.frank@tuhh.de>
2018-11-07 21:31:48 +0100

    #1 Added files for Ex01

---
+++ ex01/blatt01_1.c
@@ -0,0 +1,25 @@
+// Include the standard input output libray for printing.
+#include <stdio.h>
+
+// This is the entry point of the program.
+int main() {
+  // Declares that a and b should be of type integer.
+  int a, b; 
+
+  // Outputs "Hallo TUTOR" to the terminal with two line breaks in front and after it.
+  printf("\n\n Hallo TUTOR! \n\n");
+
+  // The value of a is set to be 22.
+  a = 22;
+
+  // And the value of b is set to be 20.
+  b = 20;
+
+  // Outputs the result of a + b as digit with special indentation of one (1) tab.
+  printf("Die Antwort ist: \t %d \n\n", a + b);
+
+    // Returns 0, to satisfy the main functions requirement to return an int.
+    // The 0 indicates that the program ran successfully.
+  return 0;
+
+} // This is the end of the main function.

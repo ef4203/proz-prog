56d819943b7495e98f60463a172cff795d756110
Luca M <luca2910@gmail.com>
2018-11-12 21:35:06 +0100

    #3 First Praesenzaufgabe solved

--- ex03/extra/extra1.c
+++ ex03/extra/extra1.c
@@ -1,5 +1,14 @@
-main(int argc, char const *argv[])
-{
-    /* code */
+// Standard library einbeziehen
+#include <stdio.h>
+// Hier fängt das Programm an
+int main() {
+// 1 - 100 welche Zahlen sind durch 3 teilbar
+    for (int num = 1; num < 101; num++)
+    {
+        if(num%3==0)
+        {
+            printf("%d ", num);
+        }
+    }
     return 0;
 }

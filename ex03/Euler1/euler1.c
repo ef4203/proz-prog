60cdc734d83495924d89119af5926b3fe24a7a23
Elias Frank <elias_frank@hotmail.com>
2018-11-13 21:30:59 +0100

    #3 Added comments to euler1.

--- ex03/Euler1/euler1.c
+++ ex03/Euler1/euler1.c
@@ -3,16 +3,22 @@
 
 int main()
 {
+    // Declares variables used in the program.
     float e, x;
     int i = 1;
 
+    // Start a loop.
     do
     {
-        x = 1 / factorial(i);
+        // Calculate the step.
+        x = 1 / factorial(i); // This function comes from common.c
+        // Add step to e.
         e += x;
+        // Increase step counter.
         i++;
-    } while (x > 0);
+    } while (x > 0); // Until x is 0.
 
+    // Print the result.
     printf("e is approx.: %.16f\n", e);
 
     return 0;

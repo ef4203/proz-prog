b9b987b351272229fe4ee0a4933ba5aac8465bda
Elias Frank <elias_frank@hotmail.com>
2018-11-13 21:01:57 +0100

    #3 Optmised euler1

--- ex03/Euler1/euler1.c
+++ ex03/Euler1/euler1.c
@@ -1,46 +1,26 @@
 #include <stdio.h>
 
-float factorial_float(int n)
+double factorial(int n)
 {
-    float result = 1;
-
-    for (int c = 1; c <= n; c++)
-    {
-        result = result * c;
-    }
-
-    return result;
+    if (n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
 }
 
-double factorial_double(int n)
-{
-    double result = 1;
-
-    for (int c = 1; c <= n; c++)
-    {
-        result = result * c;
-    }
-
-    return result;
-}
 int main()
 {
-    float e_float = 1;
+    float e, x;
+    int i = 1;
 
-    for (int i = 1; i < 100; i++)
+    do
     {
-        e_float += 1.0 / factorial_float(i);
-    }
+        x = 1 / factorial(i);
+        e += x;
+        i++;
+    } while (x > 0);
 
-    printf("e is approx. = %.20f\n", e_float);
+    printf("e is approx.: %.16f\n", e);
 
-    double e_double = 1;
-
-    for (int i = 1; i < 100; i++)
-    {
-        e_double += 1.0 / factorial_double(i);
-    }
-
-    printf("e is approx. = %.20f\n", e_double);
     return 0;
 }

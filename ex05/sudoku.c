800edb2ce152362a4ff98d82428462e3ed7bc703
Elias F <elias.frank@tuhh.de>
2018-11-22 22:15:32 +0100

    #5 Added print board method and clear screen

--- ex05/sudoku.c
+++ ex05/sudoku.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+void clrscr()
+{
+    system("@cls||clear");
+}
+
+void printboard()
+{
+    printf("    1 2 3   4 5 6   7 8 9\n");
+    printf("  -------------------------\n");
+    for (int i = 1; i <= 9; i++)
+    {
+        printf("%d | x x x | x x x | x x x |\n", i);
+        if (i == 3 || i == 6)
+        {
+            printf("  -------------------------\n");
+        }
+    }
+    printf("  -------------------------\n");
+}
 int main()
 {
-    printf("SUDOKU!\n");
+    printboard();
     return 0;
 }

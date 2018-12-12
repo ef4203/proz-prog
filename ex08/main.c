1967f15b4bea3439503b6e7bf7ccd885604026ab
Elias F <elias.frank@tuhh.de>
2018-12-12 14:32:33 +0100

    #8 Added printfs

--- ex08/main.c
+++ ex08/main.c
@@ -1,5 +1,12 @@
+#include <stdio.h>
+
 int main()
 {
-    /* code */
+    printf("1) Set array size\t\t\t4) Bubble sort\n");
+    printf("2) Input a number\t\t\t5) Merge sort\n");
+    printf("3) Generate random number\t\t6) Exit\n\n");
+    printf("> ");
+    scanf("%d");
+
     return 0;
 }

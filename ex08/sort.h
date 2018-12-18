8d6d4cd94ebb09c30370238d3a15f418f1bb870f
Elias Frank <elias.frank@tuhh.de>
2018-12-18 23:48:00 +0100

    #8 Renamed images, and added comments

--- ex08/sort.h
+++ ex08/sort.h
@@ -1,10 +1,15 @@
 #ifndef __sort_h__
 #define __sort_h__
 
-/* Sorts an array with the merge sort algorithm. */
+/* Sorts an array with the merge sort algorithm.
+ * arr - the array to sort.
+ * i   - the start value to sort from.
+ * j   - the array length. */
 int *merge_sort(int *arr, int i, int j);
 
-/* Sorts an array with the bubble sort algorithm. */
+/* Sorts an array with the bubble sort algorithm.
+ * arr - the array to sort.
+ * n   - the array length. */
 int *bubble_sort(int *arr, int n);
 
 #endif

84374a6760127cb4aa9c68c175976dac69466e9c
Elias F <32647755+ef4203@users.noreply.github.com>
2019-11-11 15:08:51 +0100

    Ipmroved spelling for ex02 and removed redudant files

--- ex02/blatt02_2.c
+++ ex02/blatt02_2.c
@@ -6,15 +6,15 @@
 /* Entry point of the programm. */
 int main()
 {
-    /* Save the to int to convert into a temporary variable */
+    /* Save the number to convert into a temporary variable */
     uint16_t x = CONVERT;
     /* Initialize a string for the result. */
     char result[] = "0000000000000000\0";
 
-    /* Loop through each bit of the binary number. */
+    /* Loop through each bit of a binary number. */
     for (int i = 15; i >= 0; i--)
     {
-        /* Se the current bit to 2^i. */
+        /* Set the current bit to 2^i. */
         int bit = pow(2, i);
         /* Check if the temporary number is greater
          * or equal to the current bit. */
